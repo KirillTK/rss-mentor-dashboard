3abfab95e74d0d4a7a5ebe676feb587288d2989e
Kirill Tkachov <35868628+KirillTK@users.noreply.github.com>
2019-02-24 18:37:13 +0300

    feature: upload project

---
+++ precache-manifest.cf4c94c78ec2f00e956bfef680a65e6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77511caa92db40fe231d",
+    "url": "/rss-mentor-dashboard/static/js/runtime~main.77511caa.js"
+  },
+  {
+    "revision": "3f545c2f6981513b7478",
+    "url": "/rss-mentor-dashboard/static/js/main.3f545c2f.chunk.js"
+  },
+  {
+    "revision": "cbe0f357a24ece282440",
+    "url": "/rss-mentor-dashboard/static/js/1.cbe0f357.chunk.js"
+  },
+  {
+    "revision": "3f545c2f6981513b7478",
+    "url": "/rss-mentor-dashboard/static/css/main.95d6a49e.chunk.css"
+  },
+  {
+    "revision": "d910177f5dc07fb06219cc9f5cb45049",
+    "url": "/rss-mentor-dashboard/index.html"
+  }
+];
